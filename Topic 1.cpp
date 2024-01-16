f8cb2aec340bb59b07a66628893ebe37ac495f1b
Farhana Bente Islam <21303195@iubat.edu>
2024-01-17 01:46:21 +0600

    Primality Test

---
+++ Topic 1.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using  namespace  std;
+
+// PRIME NUMBER TEST//
+bool prime(int n)
+{
+    if(n<2)return false;
+    if(n<=3)return true;
+    if(n%2==0)return false;
+    for(int i=3;i*i<=n;i+=2)
+    {
+        if(n%i==0)return false;
+    }
+    return true;
+}
+
+// Graph Grid //
+int dx[]={-1,0,1,0};
+int dy[]={0,1,0,-1};
+
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        if(prime(n)==true)cout<<"Prime"<<endl;
+        else cout<<"Not Prime"<<endl;
+    }
+}
